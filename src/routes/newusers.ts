b08b4c491f154fb49a543bb7e10ac577bda292ad
Abhishek Khangare <abhi@gmail.com>
2024-08-12 19:16:09 +0530

    update newusers.ts

--- src/routes/newusers.ts
+++ src/routes/newusers.ts
@@ -16,7 +16,6 @@ router.get("/me", verifyToken, async (req: Request, res: Response) => {
     }
     res.json(user);
   } catch (error) {
-    console.log(error);
     res.status(500).json({ message: "something went wrong" });
   }
 });
